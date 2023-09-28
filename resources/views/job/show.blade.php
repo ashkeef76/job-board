f605276ab017c802decdfa81ae2a474cef9d12a2
moneim <ashkeef76@gmail.com>
2023-09-29 00:40:32 +0300

    first commit

---
+++ resources/views/job/show.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-job-card :$job>
+        <div>
+            <x-link-button :href=" route('jobs.index') ">
+                back
+            </x-link-button>
+
+        </div>
+    </x-job-card>
+
+</x-layout>
